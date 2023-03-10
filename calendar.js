43c12a0f8b81527b2b5ebeba93b4f1138ec8d2c3
Alice Mao <amao@wustl.edu>
2023-03-09 23:53:42 -0600

    made even more changes

--- calendar.js
+++ calendar.js
@@ -1,27 +1,27 @@
-document.getElementById("create").addEventListener("click", popup, false);
+// document.getElementById("create").addEventListener("click", popup, false);
 
-function popup() {
-    console.log("asdf");
+// function popup() {
+//     console.log("asdf");
 
-    var element = document.getElementById("createPopup");
-    var style = element.style.display;
-    if (element.style.display == "block") {
-        element.style.display == "none";
-    }
-    else {
-        element.style.display = "block";
-    }
+//     var element = document.getElementById("createPopup");
+//     var style = element.style.display;
+//     if (element.style.display == "block") {
+//         element.style.display == "none";
+//     }
+//     else {
+//         element.style.display = "block";
+//     }
 
-    document.getElementById("x").addEventListener("click", closePopup, false);
+//     document.getElementById("x").addEventListener("click", closePopup, false);
 
-}
+// }
 
-function closePopup() {
-    var element = document.getElementById("createPopup");
-    element.innerHTML = "<div id='x'>x</div>";
-    element.style.display = "none";
-    document.getElementById("login").addEventListener("click", registerPopup, false);
-}
+// function closePopup() {
+//     var element = document.getElementById("createPopup");
+//     element.innerHTML = "<div id='x'>x</div>";
+//     element.style.display = "none";
+//     document.getElementById("login").addEventListener("click", registerPopup, false);
+// }
 
 
 
@@ -30,7 +30,7 @@ function Week(c) { this.sunday = c.getSunday(); this.nextWeek = function () { re
 function Month(c, b) { this.year = c; this.month = b; this.nextMonth = function () { return new Month(c + Math.floor((b + 1) / 12), (b + 1) % 12) }; this.prevMonth = function () { return new Month(c + Math.floor((b - 1) / 12), (b + 11) % 12) }; this.getDateObject = function (a) { return new Date(this.year, this.month, a) }; this.getWeeks = function () { var a = this.getDateObject(1), b = this.nextMonth().getDateObject(0), c = [], a = new Week(a); for (c.push(a); !a.contains(b);)a = a.nextWeek(), c.push(a); return c } };
 
 const date = new Date();
-var currentMonth = new Month((date.getFullYear()), (date.getMonth())); // October 2017
+let currentMonth = new Month((date.getFullYear()), (date.getMonth())); // October 2017
 Window.onload = updateCalendar();
 
 // Change the month when the "next" button is pressed
@@ -63,7 +63,6 @@ function updateCalendar() {
     document.getElementById("month").textContent = monthName;
     document.getElementById("year").textContent = year;
 
-
     var weeks = currentMonth.getWeeks();
 
     let divs = document.getElementsByClassName("grid-item");
@@ -79,25 +78,24 @@ function updateCalendar() {
             // WebWit Inspector, or Dragonfly.
             let currDay = days[d].getDate();
 
-            if(currDay == 1){
+            if (currDay == 1) {
                 oneCounter++;
             }
 
-            if(oneCounter == 0){
+            if (oneCounter == 0) {
                 // dispaly light green
                 divs[i].style.backgroundColor = "#A6BB8D";
             }
-            else if(oneCounter == 1){
+            else if (oneCounter == 1) {
                 divs[i].style.backgroundColor = "#61876E";
             }
-            else{
+            else {
                 // display light green
                 divs[i].style.backgroundColor = "#A6BB8D";
             }
 
             divs[i].innerHTML = days[d].getDate();
             i++;
-
         }
     }
 
@@ -119,7 +117,6 @@ function updateCalendar() {
 }
 
 // ajax.js
-
 function registerAjax() {
     const first_name = document.getElementById("first_name").value; // Get the username from the form
     const last_name = document.getElementById("last_name").value; // Get the password from the form
@@ -146,24 +143,129 @@ function registerAjax() {
 }
 
 
-
-
-
-
-
 //listener and function to create the registration popup
-document.getElementById("login").addEventListener("click", registerPopup, false);
+// document.getElementById("login").addEventListener("click", registerPopup, false);
 
-function registerPopup() {
-    document.getElementById("login").removeEventListener("click", registerPopup, false);
-    popup();
-    let popupElement = document.getElementById("createPopup");
-
-
-    popupElement.innerHTML += "<div class='box'><form method='POST'><br><label>First Name: </label><br><input type='text' id='first_name' name='first_name'><br><label>Last Name: </label><br><input type='text' id='last_name' name='last_name'><br><label>Username: </label><br><input type='text' id='username' name='username'><br><label>Email Address: </label><br><input type='email' id='email' name='email'><br><label>Password: </label><br><input type='password' id='pwd' name='pwd'><br><label>Retype Password:</label><br><input type='password' id='pwd_check' name='checkpwd'><br><br><br><br><input class='button' type='submit' value='Register' name='submit' id='registersubmit'><br><br></form></div> ";
-    document.getElementById("x").addEventListener("click", closePopup, false);
-    document.getElementById("registersubmit").addEventListener("click", registerAjax, false); // Bind the AJAX call to button click
+// function registerPopup() {
+//     document.getElementById("login").removeEventListener("click", registerPopup, false);
+//     popup();
+//     let popupElement = document.getElementById("createPopup");
 
 
+//     // popupElement.innerHTML += "<div class='box'><form method='POST'><br><label>First Name: </label><br><input type='text' id='first_name' name='first_name'><br><label>Last Name: </label><br><input type='text' id='last_name' name='last_name'><br><label>Username: </label><br><input type='text' id='username' name='username'><br><label>Email Address: </label><br><input type='email' id='email' name='email'><br><label>Password: </label><br><input type='password' id='pwd' name='pwd'><br><label>Retype Password:</label><br><input type='password' id='pwd_check' name='checkpwd'><br><br><br><br><input class='button' type='submit' value='Register' name='submit' id='registersubmit'><br><br></form></div> ";
+//     // document.getElementById("x").addEventListener("click", closePopup, false);
+//     document.getElementById("registersubmit").addEventListener("click", registerAjax, false); // Bind the AJAX call to button click
+// }
+
+
+
+// ADD EVENTLISTENER TO EVERY ELEMENT IN GRID, IF IT IS CLICKED EXECUTE FUNCTION
+Window.onload = setEventListeners();
+
+function setEventListeners() {
+
+    let gridArray = document.getElementsByClassName("grid-item");
+    let sideBar = document.getElementById("dateTitle");
+    let displayCurrentMonth = currentMonth.getDateObject;
+    let previousElement = '14';
+    let previousDay = 15;
+
+    for (let i = 0; i < gridArray.length; i++) {
+        gridArray[i].addEventListener('click', e => {
+
+            let displayCurrentYear = currentMonth.year;
+
+            // CONVERT ID TO INT TO COMPARE LATER ON
+
+            let currentElement = e.target.id;
+            let currentElementId = parseInt(currentElement);
+
+            // GETTING THE DAY AND CONVERTING IT TO AN INT
+            let displayCurrentDay = e.target.textContent;
+            let currentDayAsInt = parseInt(displayCurrentDay);
+
+            // IF THE DAY CLICKED WAS IN THE PREVIOUS MONTH
+            if (currentElementId < 8 && currentDayAsInt > 7) {
+
+                // CHANGE THE MONTH TO BE THE PREVIOUS MONTH
+                displayCurrentMonth = currentMonth.prevMonth().month;
+
+                // IF THE NEW MONTH IS DECEMBER, CHANGE THE YEAR
+                if (displayCurrentMonth == 11) {
+                    displayCurrentYear = currentMonth.year - 1;
+                }
+            }
+
+            // IF THE DAY CLICKED WAS IN THE NEXT MONTH
+            else if (currentElementId > 28 && currentDayAsInt < 15) {
+
+                // CHANGE MONTH TO BE THE NEXT MONTH
+                displayCurrentMonth = currentMonth.nextMonth().month;
+
+                // IF THE NEW MONTH IS JANUARY, CHANGE THE YEAR
+                if (displayCurrentMonth == 0) {
+                    displayCurrentYear = currentMonth.year + 1;
+                }
+            }
+
+            // IF THE DAY CLICKED WAS IN THE CURRENT MONTH
+            else {
+                displayCurrentMonth = currentMonth.month;
+                displayCurrentYear = currentMonth.year;
+            }
+
+            // CONVERT MONTH NUMBER TO STRING
+            date.setMonth(displayCurrentMonth);
+            let monthName = date.toLocaleString("en-US", {
+                month: 'long',
+            })
+
+
+            sideBar.textContent = monthName + " " + displayCurrentDay + ", " + displayCurrentYear;
+
+            // CONTROL THE COLOR OF THE GRID BLOCK
+            document.getElementById(currentElement).style.backgroundColor = "#3C6255";
+
+
+            // CHANGE THE PREVIOUS DAY NOT IN THE CURRENT MONTH TO LIGHT GREEN
+            if ((previousElement < 8 && previousDay > 15) || (previousElement > 28 && previousDay < 15)) {
+                console.log("current id: " + currentElementId + "  currentDay: " + currentDayAsInt + "  previ: " + previousElement);
+                document.getElementById(previousElement).style.backgroundColor = "#A6BB8D";
+            }
+
+            /// CHANGE THE PREVIOUS DAY IN CURRENT MONTH BACK TO GREEN
+            else {
+                document.getElementById(previousElement).style.backgroundColor = "#61876E";
+            }
+
+            previousDay = displayCurrentDay;
+            previousElement = currentElement;
+
+            // CONTROL WHAT IS SHOWN ON THE SIDE
+            document.getElementById("addForm").style.display = "none";
+            document.getElementById("loginForm").style.display = "none";
+            document.getElementById("registerForm").style.display = "none";
+            document.getElementById("currentDateEvents").style.display = "block";
+        }, false);
+    }
+}
+
+
+// WHEN ADD EVENT FORM BUTTON HAS BEEN CLICKED
+document.getElementById("addEventButton").addEventListener('click', displayAddForm, false);
+
+function displayAddForm() {
+
+    // SHOW ADD EVENT FORM, HIDE EVENTS FROM SELECTED DAY
+    if (document.getElementById("addForm").style.display == "none") {
+        document.getElementById("addForm").style.display = "block";
+        document.getElementById("currentDateEvents").style.display = "none";
+    }
+
+    // HIDE ADD EVENT FORM, SHOW EVENTS FROM SELECTED DAY
+    else {
+        document.getElementById("addForm").style.display = "none";
+        document.getElementById("currentDateEvents").style.display = "block";
+    }
 }
 
